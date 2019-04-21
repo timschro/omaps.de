599a4171337a8591d20c905db1a923302f5cc7f8
Tim Schröder <tim.schroeder@publicispixelpark.de>
2019-04-21 22:46:11 +0200

    Fixing abilities for dashboard

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,11 @@ class Ability
   def initialize(user)
     if user.present?
       can :read, Map
+      can :new, Map
       can :read, Club
       can :read, MapType
       can :new, Club
+      can :dashboard, :all
 
       can :manage, Map, submitter_id: user.id
 
@@ -15,14 +17,12 @@ class Ability
 
       if user.admin?
         can :manage, [Map, MapType, Club]
-        can :dashboard, [Map, MapType, Club]
         can :history, [Map, MapType, Club]
       end
 
       if user.superadmin?
         can :read, :all
         can :manage, :all
-        can :dashboard, :all
         can :history, :all
       end
 
