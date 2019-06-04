2428c37d441b3435453685019a2fb9aff72d0a37
Tim Schröder <tim.schroeder@publicispixelpark.de>
2019-06-04 22:53:08 +0200

    Trying new Mailer Domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { :host => "www.omaps.de" }
+
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
