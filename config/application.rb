a60e690300ee4457fb2e941d46d32e0730778bdb
Tim Schröder <tim.schroeder@publicispixelpark.de>
2019-12-19 09:42:12 +0100

    Adding middleware

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Omaps
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.middleware.use ActionDispatch::Session::CookieStore, {:key=>"_omaps_session", :cookie_only=>true}
+    
   end
 end
